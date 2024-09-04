a49f83c596e9e2d613709395f2002eb4ef525b08
MichaelGrabinski <mikeyboy06898@gmail.com>
2024-09-04 11:10:21 -0400

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.AllowAny',),
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
 }
 
