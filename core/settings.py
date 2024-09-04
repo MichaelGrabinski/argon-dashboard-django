a709c8d2bd02af567e640f53b709ae4a0a8e653a
MichaelGrabinski <mikeyboy06898@gmail.com>
2024-09-04 14:45:26 -0400

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 # load production server from .env
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'linux.krustykrew.org', config('SERVER', default='127.0.0.1')]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'linux.krustykrew.org','humanfutures-djgch2ghdhaycshg.eastus2-01.azurewebsites.net'' ,config('SERVER', default='127.0.0.1')]
 
 # Application definition
 
