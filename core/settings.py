b4ba6e0f9662ba812f98cc9c3414cc8001470e8f
MichaelGrabinski <mikeyboy06898@gmail.com>
2024-09-04 15:46:15 -0400

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -76,12 +76,27 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+
+import os
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('humanfuturesserver'),
+            'USER': os.getenv('mkzjjytnyr'),
+            'PASSWORD': os.getenv('8NzclMf$oMQ3EG6L'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT', '5432'),  # Default to '5432' if not set
+        }
     }
-}
+
+
+#DATABASES = {
+#   'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': 'db.sqlite3',
+#    }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
